5e9b00be90bec8dc1f5417518022fe67f11a2ceb
Pedro Barros <pedrobars7a@gmail.com>
2023-01-23 17:24:45 -0300

    Overview component text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from './src/components/Text';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text size={24} weight='600' opacity={0.5}>Open up App.tsx to start working on your app!</Text>
+      <Text weight='400'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
