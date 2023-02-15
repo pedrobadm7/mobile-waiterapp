e2befeff1f93ff71db2a9fc6c5c8b7f2086b77d9
Pedro Barros <pedrobars7a@gmail.com>
2023-02-15 11:29:07 -0300

    style: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { Main } from './src/components/Main';
-
+import * as SplashScreen from 'expo-splash-screen';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import { useEffect} from 'react';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,6 +14,17 @@ export default function App() {
     'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
     'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
   });
+
+  useEffect(() => {
+    async function prepare() {
+      if (fontsLoaded) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
 
